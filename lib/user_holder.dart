4d9568d18400c928f4d05167764f2e03d71d316d
Mikhail Ivanou <agidel2012@gmail.com>
2020-08-05 12:46:34 +0300

    lesson code

---
+++ lib/user_holder.dart
@@ -0,0 +1,16 @@
+import 'models/user.dart';
+
+class UserHolder {
+  Map<String, User> users = {};
+
+  void registerUser(String name, String phone, String email) {
+    User user = User(name: name, phone: phone, email: email);
+
+    if (!users.containsKey(user.login)) {
+      users[user.login] = user;
+    } else {
+      throw Exception('A user with ths name already exists');
+    }
+  }
+
+}
